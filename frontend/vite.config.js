3beb92b223097e64a3f386930e5b8240f76ee908
Harini1014 <harinithanagraj10@gmail.com>
2026-07-16 14:22:53 +0530

    Frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   server: {
     port: 5173,
     proxy: {
-      '/validate': 'https://tool-2-3w1t.onrender.com/,
-      '/health': 'https://tool-3-vctq.onrender.com,
+      '/validate': 'https://tool-2-3w1t.onrender.com/',
+      '/health': 'https://tool-3-vctq.onrender.com',
     }
   }
 })
